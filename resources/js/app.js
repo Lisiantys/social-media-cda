c22bce503e46e116301894f0e1bcd248e4ed4304
Lisiantys <maxime.pouzargue7@gmail.com>
2024-06-04 12:21:17 +0200

    V0.2 - Installation de VueJs
    Le projet est maintenant configuré pour être utilisé avec VueJs;

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import { createApp } from "vue"
+
+import App from "./components/App.vue"
+
+import router from './routeur'
+
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+createApp(App).use(router).use(pinia).mount("#app")
